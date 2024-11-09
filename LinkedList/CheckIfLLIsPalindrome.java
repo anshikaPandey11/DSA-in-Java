5318b8db3f50a582240ba0b1fb04b9288c419632
Anshika Pandey <anskikaallen0203@gmail.com>
2024-11-09 16:28:33 +0530

    LinkedList

---
+++ LinkedList/CheckIfLLIsPalindrome.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+public class CheckIfLLIsPalindrome {
+    // Steps to check ll
+    // find midNode = slow(turtle)=head //+1 & fast(hare)=head //+2
+    // 2nd half reverse
+    // check if 1st half=2nd half
+
+    public static class Node {
+        int data;
+        Node next;
+
+        // constructor
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // Add element at last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+
+    }
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+        }
+        return slow; // slow is middle
+    }
+
+    public boolean checkPalindrome() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node midNode = findMid(head);
+        // reverse of second half
+
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev; // right half head
+        Node left = head;
+
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    // print Linked List
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        CheckIfLLIsPalindrome ll = new CheckIfLLIsPalindrome();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        ll.addLast(1);
+        ll.print();
+        System.out.println(ll.checkPalindrome());
+    }
+
+}
